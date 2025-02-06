81f8ce604e3ba72c58d63c6c338fc0158cb5be7c
AlvaroCS2020 <alvaro8imaz@gmail.com>
2025-02-06 16:06:20 -0300

    PORFIN deteccion de todas las esquinas del tablero

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-﻿# SCARA robot arm, to play ches!!
+﻿# SCARA robot arm, to play chess!!
 This robot and system aims to be able to play a regular chess match against any human.
 
 ## CAD
@@ -6,5 +6,10 @@ This robot and system aims to be able to play a regular chess match against any 
 
 ## OpenCv Auto homing demo
 ![Alt Text](autoHomingDemo.gif)
+
+## OpenCv board detection demo
+By modyfing the output of the function findChessboardCorners() I got to map all the squares of a regular chess board
+
+![Alt Text](boardDetectionDemo.gif)
 
 
--- boardProcess.py
+++ boardProcess.py
@@ -1,16 +1,17 @@
 # import required libraries
 import cv2
 import numpy as np
+import time
 cap = cv2.VideoCapture(1)
 ret, frame = cap.read()
 doOnlyOnce = True
 first_corners = []
+COUNTER_TEST = 0
 def addCorners(corners, image):
-    ##PUNTOS FINALES E INICIALES
-    x, y = corners[0][0]  # Extrae los valores
-    image = cv2.circle(image, (int(x), int(y)), 8, (0,0,120), 2)
-    x, y = corners[len(corners)-1][0]  # Extrae los valores
-    image = cv2.circle(image, (int(x), int(y)), 8, (0,0,255), 2)
+    global COUNTER_TEST
+    newCorners = np.empty((0, 1, 2)) 
+
+    
     red = 50
     rows, cols = 7, 7
     dx = 0
@@ -31,11 +32,12 @@ def addCorners(corners, image):
             magnitude = np.linalg.norm((vector_next - vector))
             count += 1
             dx += magnitude
-        image = cv2.circle(image, (int(x), int(y)), 5, (red,red,red), 2)
+        #image = cv2.circle(image, (int(x), int(y)), 5, (red,red,red), 2)
         #print(str(y) + " : " + str(x))
     dx = dx/count
-    print(str(dx))
-    adyacents = 1
+    
+    adyacents = 0
+    deltaIndex = 0
     #Agrego los puntos extra??
     for i in range(1,len(corners)): #i (0,6) 
         ##GETTING ONLY de 6TH column
@@ -44,20 +46,75 @@ def addCorners(corners, image):
             xi, yi = corners[i-6][0]  # Extrae los valores
             image = cv2.circle(image, (int(xf), int(yf)), 5, (255,0,0), 5)
             vector = np.array([[xf-xi, yf-yi]])
-            image = cv2.line(image, (int(xi), int(yi)), (int(xf), int(yf)), (0, 255, 0), 2)
+            #image = cv2.line(image, (int(xi), int(yi)), (int(xf), int(yf)), (0, 255, 0), 2)
             magnitude = np.linalg.norm(vector)
             direction = vector/magnitude #NORMALIZATION
+            
             vector = np.array([[xf, yf]])
             newPoint = vector + direction *(dx*0.6)
             xNew, yNew = newPoint[0]
-            image = cv2.circle(image, (int(xNew), int(yNew)), 2, (0,255,0), 2)
+            #image = cv2.circle(image, (int(xNew), int(yNew)), 2, (0,255,0), 2)
+            prependElement = np.array([[[xNew, yNew]]])  # Shape (1, 1, 2)
             vector = np.array([[xi, yi]])
+            
             newPoint = vector - direction *(dx*0.6)
             xNew, yNew = newPoint[0]
-            image = cv2.circle(image, (int(xNew), int(yNew)), 2, (0,255,0), 2)
+            appendElement = np.array([[[xNew, yNew]]])  # Shape (1, 1, 2)
+            slice_arr = corners[i-6:i+1]  # Shape (n, 1, 2)
+            slice_arr = np.concatenate((appendElement, slice_arr, prependElement), axis=0) 
+            print("len "+str(len(slice_arr)))           
+            newCorners = np.concatenate((newCorners,slice_arr), axis=0)
+            #newCorners = np.insert(newCorners, (i + deltaIndex) -7 , new_tuple, axis=0)
+            #image = cv2.circle(image, (int(xNew), int(yNew)), 2, (0,255,0), 2)
+    #Agrego los puntos extra??
+    fstSlice = newCorners[0:9].copy()
     
+    for i in range(0,len(fstSlice)):
+        xf, yf = fstSlice[i][0] 
+        xi, yi = newCorners[9*6+i][0] 
+        vector = np.array([[xf-xi, yf-yi]]) 
+
+        magnitude = np.linalg.norm(vector)
+        
+        direction = vector/magnitude #NORMALIZATION
+        print(np.linalg.norm(direction))
+        vector = np.array([[xf, yf]]) 
+        newPoint = vector + direction *(dx*0.6)
+        xNew, yNew = newPoint[0]
+        fstSlice[i][0] = xNew, yNew 
+        #image = cv2.line(image, (int(xi), int(yi)), (int(xf), int(yf)), (0, 255, 0), 2)
+    #append pending
+    lastSlice = newCorners[(len(newCorners)-9):(len(newCorners))].copy()
     
+    for i in range(0,len(lastSlice)):
+        xf, yf = lastSlice[i][0] 
+        xi, yi = newCorners[len(newCorners)-(9*7-i)][0] 
+        image = cv2.line(image, (int(xi), int(yi)), (int(xf), int(yf)), (0, 255, 0), 2)
+        
+        vector = np.array([[xf-xi, yf-yi]]) 
+        
+        magnitude = np.linalg.norm(vector)
+        
+        direction = vector/magnitude #NORMALIZATION
+        vector = np.array([[xf, yf]]) 
+        newPoint = vector + direction *(dx*0.6)
+        xNew, yNew = newPoint[0]
+        lastSlice[i][0] = xNew, yNew 
+    print(lastSlice)
+    newCorners = np.concatenate((fstSlice,newCorners, lastSlice), axis=0)
+    COUNTER_TEST = (COUNTER_TEST + 1) %len(newCorners) 
     
+    for i in range(0,COUNTER_TEST): #i (0,6) 
+        x, y = newCorners[i][0]  # Extrae los valores
+        image = cv2.circle(image, (int(x), int(y)), 5, (255,255,red), 2)
+    
+    #print(len(newCorners))
+    #print(newCorners)
+    ##PUNTOS FINALES E INICIALES
+    x, y = newCorners[0][0]  # Extrae los valores
+    image = cv2.circle(image, (int(x), int(y)), 8, (0,0,120), 2)
+    x, y = newCorners[len(newCorners)-1][0]  # Extrae los valores
+    image = cv2.circle(image, (int(x), int(y)), 8, (0,0,255), 2)
     return image 
 
 while not ret:
@@ -89,4 +146,6 @@ while True:
         #img = cv2.drawChessboardCorners(img, (n,n), corners,ret)
     if not doOnlyOnce:
         img = addCorners(first_corners, img)
+        time.sleep(0.1)
+        pass
     cv2.imshow('Chessboard',img)
