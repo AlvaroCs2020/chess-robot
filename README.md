996059af6a6f24cb590cf5e2935bbc575a840695
AlvaroCS2020 <alvaro8imaz@gmail.com>
2025-02-07 18:11:07 -0300

    Movement detection 'done' major refactor gotta be done

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ This robot and system aims to be able to play a regular chess match against any 
 ![Alt Text](autoHomingDemo.gif)
 
 ## OpenCv board detection demo
-By modyfing the output of the function findChessboardCorners() I got to map all the squares of a regular chess board
+By modyfing the output of the function findChessboardCorners(), using numpy, I got to map all the squares of a regular chess board
 
 ![Alt Text](boardDetectionDemo.gif)
 
+## OpenCv movement detection demo
+Here I am comparing the MSE between every current square of the board agains every square of a pic where the board was empy, allowing the system to detect wich square has changed the most
+![Alt Text](movementDetectionDemo.gif)
 
--- boardProcess.py
+++ boardProcess.py
@@ -6,14 +6,52 @@ cap = cv2.VideoCapture(1)
 ret, frame = cap.read()
 doOnlyOnce = True
 first_corners = []
-COUNTER_TEST = 0
+slicesFst = []
+def extract_region(image, points):
+    """
+    Extracts a region from the image based on four given points.
+    
+    Parameters:
+        image (numpy.ndarray): The input image.
+        points (list of tuples): Four points (x, y) defining the region.
+    
+    Returns:
+        numpy.ndarray: The extracted region.
+    """
+    # Define the destination size based on the bounding box of the points
+    width = int(max(np.linalg.norm(points[0] - points[1]), np.linalg.norm(points[2] - points[3])))
+    height = int(max(np.linalg.norm(points[0] - points[3]), np.linalg.norm(points[1] - points[2])))
+    
+    dst_points = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]], dtype=np.float32)
+    
+    # Compute the perspective transformation matrix
+    matrix = cv2.getPerspectiveTransform(np.array(points, dtype=np.float32), dst_points)
+    
+    # Apply the transformation
+    extracted_region = cv2.warpPerspective(image, matrix, (width, height))
+    
+    return extracted_region
+def saveSlices(image, corners):
+    slices = []
+    coordinates = []
+    for i in range(0,len(corners)-10):
+        #points = np.array([(100, 100), (400, 100), (400, 300), (100, 300)])
+        x1, y1 = corners[i][0]
+        x2, y2 = corners[i+1][0]
+        x3, y3 = corners[i+9][0]
+        x4, y4 = corners[i+10][0]
+        points = np.array([(x3, y3), (x4, y4), (x2, y2), (x1, y1)])
+        extracted = extract_region(image, points)
+        slices.append(extracted)
+        coordinates.append(points)
+    return slices, coordinates
+        
+
 def addCorners(corners, image):
-    global COUNTER_TEST
     newCorners = np.empty((0, 1, 2)) 
 
     
     red = 50
-    rows, cols = 7, 7
     dx = 0
     count = 0
     #Ugly Math
@@ -35,16 +73,14 @@ def addCorners(corners, image):
         #image = cv2.circle(image, (int(x), int(y)), 5, (red,red,red), 2)
         #print(str(y) + " : " + str(x))
     dx = dx/count
-    
-    adyacents = 0
-    deltaIndex = 0
+
     #Agrego los puntos extra??
     for i in range(1,len(corners)): #i (0,6) 
         ##GETTING ONLY de 6TH column
         if(i % 7 == 6):    #crazy ass math 
             xf, yf = corners[i][0]  # Extrae los valores
             xi, yi = corners[i-6][0]  # Extrae los valores
-            image = cv2.circle(image, (int(xf), int(yf)), 5, (255,0,0), 5)
+            #image = cv2.circle(image, (int(xf), int(yf)), 5, (255,0,0), 5)
             vector = np.array([[xf-xi, yf-yi]])
             #image = cv2.line(image, (int(xi), int(yi)), (int(xf), int(yf)), (0, 255, 0), 2)
             magnitude = np.linalg.norm(vector)
@@ -62,7 +98,7 @@ def addCorners(corners, image):
             appendElement = np.array([[[xNew, yNew]]])  # Shape (1, 1, 2)
             slice_arr = corners[i-6:i+1]  # Shape (n, 1, 2)
             slice_arr = np.concatenate((appendElement, slice_arr, prependElement), axis=0) 
-            print("len "+str(len(slice_arr)))           
+            #print("len "+str(len(slice_arr)))           
             newCorners = np.concatenate((newCorners,slice_arr), axis=0)
             #newCorners = np.insert(newCorners, (i + deltaIndex) -7 , new_tuple, axis=0)
             #image = cv2.circle(image, (int(xNew), int(yNew)), 2, (0,255,0), 2)
@@ -77,7 +113,7 @@ def addCorners(corners, image):
         magnitude = np.linalg.norm(vector)
         
         direction = vector/magnitude #NORMALIZATION
-        print(np.linalg.norm(direction))
+        #print(np.linalg.norm(direction))
         vector = np.array([[xf, yf]]) 
         newPoint = vector + direction *(dx*0.6)
         xNew, yNew = newPoint[0]
@@ -89,7 +125,7 @@ def addCorners(corners, image):
     for i in range(0,len(lastSlice)):
         xf, yf = lastSlice[i][0] 
         xi, yi = newCorners[len(newCorners)-(9*7-i)][0] 
-        image = cv2.line(image, (int(xi), int(yi)), (int(xf), int(yf)), (0, 255, 0), 2)
+        #image = cv2.line(image, (int(xi), int(yi)), (int(xf), int(yf)), (0, 255, 0), 2)
         
         vector = np.array([[xf-xi, yf-yi]]) 
         
@@ -100,25 +136,44 @@ def addCorners(corners, image):
         newPoint = vector + direction *(dx*0.6)
         xNew, yNew = newPoint[0]
         lastSlice[i][0] = xNew, yNew 
-    print(lastSlice)
+
     newCorners = np.concatenate((fstSlice,newCorners, lastSlice), axis=0)
-    COUNTER_TEST = (COUNTER_TEST + 1) %len(newCorners) 
+    #
+    #COUNTER_TEST = (COUNTER_TEST + 1) %len(newCorners) 
+    #
+    #for i in range(0,COUNTER_TEST): #i (0,6) 
+    #    x, y = newCorners[i][0]  # Extrae los valores
+    #    image = cv2.circle(image, (int(x), int(y)), 5, (255,255,red), 2)
+    #
+    ##print(len(newCorners))
+    ##print(newCorners)
+    ###PUNTOS FINALES E INICIALES
+    #x, y = newCorners[0][0]  # Extrae los valores
+    #image = cv2.circle(image, (int(x), int(y)), 8, (0,0,120), 2)
+    #x, y = newCorners[len(newCorners)-1][0]  # Extrae los valores
+    #image = cv2.circle(image, (int(x), int(y)), 8, (0,0,255), 2)
     
-    for i in range(0,COUNTER_TEST): #i (0,6) 
-        x, y = newCorners[i][0]  # Extrae los valores
-        image = cv2.circle(image, (int(x), int(y)), 5, (255,255,red), 2)
-    
-    #print(len(newCorners))
-    #print(newCorners)
-    ##PUNTOS FINALES E INICIALES
-    x, y = newCorners[0][0]  # Extrae los valores
-    image = cv2.circle(image, (int(x), int(y)), 8, (0,0,120), 2)
-    x, y = newCorners[len(newCorners)-1][0]  # Extrae los valores
-    image = cv2.circle(image, (int(x), int(y)), 8, (0,0,255), 2)
-    return image 
+    return image, newCorners 
+def getBiggestError(slices, coordinates, img):
+    #get slices
+    max_score = 0
+    for i in range(0,len(slices)-1):
+        extracted = extract_region(img, coordinates[i])
+        
+        gray1 = cv2.cvtColor(extracted, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(slices[i], cv2.COLOR_BGR2GRAY)
+        
+        mse_score = int(np.mean((extracted.astype("float") - slices[i].astype("float")) ** 2))
+        if (mse_score > max_score):
+            max_score = mse_score
+            index = i
+        print(str(i) + " error: " +str(mse_score))
+    return index
+staticSlices = []
+staticCoordinates = []
+index = 0
 
 while not ret:
-    print(ret)
     ret, frame = cap.read()
 while True:
     n = 7
@@ -137,15 +192,40 @@ while True:
     if ret == True:
         if doOnlyOnce:
             first_corners = corners
-            print(type(corners))
-            print(corners[0])
-            addCorners(first_corners, img)
+            _, staticCorners = addCorners(first_corners, img)
+            staticSlices, staticCoordinates = saveSlices(img, staticCorners)#TODO hacer la comparacion frame por frame
             doOnlyOnce = False
-         
+            
         # Draw and display the corners
         #img = cv2.drawChessboardCorners(img, (n,n), corners,ret)
     if not doOnlyOnce:
-        img = addCorners(first_corners, img)
-        time.sleep(0.1)
-        pass
+       
+        
+        img, _ = addCorners(first_corners, img)
+        point = staticCoordinates[index]
+        extractedOld = staticSlices[index]
+        extracted = extract_region(img, point)
+        
+        gray1 = cv2.cvtColor(extracted, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(extractedOld, cv2.COLOR_BGR2GRAY)
+        
+        mse_score = int(np.mean((gray1.astype("float") - gray2.astype("float")) ** 2))
+        #print("Erro medio cuadratico "+str(mse_score))
+        topRight = (int(point[0][0]), int(point[0][1]))
+        bottomRight = (int(point[1][0]), int(point[1][1]))
+        bottomLeft = (int(point[2][0]), int(point[2][1]))
+        topLeft = (int(point[3][0]), int(point[3][1]))
+        # draw the bounding box of the ArUCo detection
+        # points = np.array([(x1, y1), (x2, y2), (x3, y3), (x4, y4)])
+        if cv2.waitKey(1) & 0xFF == ord('r'):
+            index = getBiggestError(staticSlices,staticCoordinates,img)
+        cv2.line(img, topLeft, topRight, (0, 255, 0), 2)
+        cv2.line(img, topRight, bottomRight, (0, 255, 0), 2)
+        cv2.line(img, bottomRight, bottomLeft, (0, 255, 0), 2)
+        cv2.line(img, bottomLeft, topLeft, (0, 255, 0), 2)
+        
+        cv2.imshow('ext',extracted)
+        
+        #time.sleep(0.1)
+        
     cv2.imshow('Chessboard',img)
